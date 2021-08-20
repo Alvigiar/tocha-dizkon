64f1ab6de6119a1281742c05b2dc46da94a7d990
Alvigiar <vladislav-medvedkov@gmail.com>
2021-08-20 18:37:05 +0300

    start project

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-import './style.css'
+import '/styles/main.scss';
+import '/node_modules/normalizecss/normalize.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const btnBurger = document.querySelector('.nav-burger')
+const btnInput = document.querySelector('.nav-btn__input')
+const modalInput = document.querySelector('.modal-input')
+const modalInputClose = document.querySelector('.input-box__btn-close')
+
+
+btnBurger.addEventListener('click', () => {
+  document.querySelector('.header-nav__content').classList.toggle('active'),
+    document.querySelector('body').classList.toggle('off'),
+    document.querySelector('.nav-burger__toggle').classList.toggle('cross')
+})
+
+btnInput.addEventListener('click', () => {
+  modalInput.classList.add('modal-input-show')
+})
+
+modalInputClose.addEventListener('click', () => {
+  modalInput.classList.remove('modal-input-show')
+})
+
