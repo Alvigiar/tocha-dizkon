eadef292199fa82c67cfbf10ad0d97453785e970
Alvigiar <vladislav-medvedkov@gmail.com>
2021-09-06 16:10:53 +0300

    add sector_2, fixed

--- main.js
+++ main.js
@@ -2,27 +2,43 @@ import './styles/main.scss';
 import './node_modules/normalizecss/normalize.css';
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const burgerBtn = document.querySelector('.header-burger')
-  const htmlBody = document.querySelector('body')
-  const modalEntranceOpen = document.querySelector('.header-cell__btn-input')
-  const modalEntranceClose = document.querySelector('.entrance-box__btn-close')
-  const entranceBox = document.querySelector('.entrance')
+// document.addEventListener('DOMContentLoaded', () => {
+//   const burgerBtn = document.querySelector('.header-burger')
+//   const htmlBody = document.querySelector('body')
+//   const modalEntranceOpen = document.querySelector('.header-cell__btn-input')
+//   const modalEntranceClose = document.querySelector('.entrance-box__btn-close')
+//   const entranceBox = document.querySelector('.entrance')
 
-  burgerBtn.addEventListener('click', () => {
-    document.querySelector('.header-content').classList.toggle('active'),
-      htmlBody.classList.toggle('off'),
-      document.querySelector('.header-burger__toggle').classList.toggle('cross')
-  })
+//   burgerBtn.addEventListener('click', () => {
+//     document.querySelector('.header-content').classList.toggle('active'),
+//       htmlBody.classList.toggle('off'),
+//       document.querySelector('.header-burger__toggle').classList.toggle('cross')
+//   })
 
-  modalEntranceOpen.addEventListener('click', () => {
-    entranceBox.classList.add('modal--show'),
-      htmlBody.classList.add('off')
-  })
+//   modalEntranceOpen.addEventListener('click', () => {
+//     entranceBox.classList.add('modal--show'),
+//       htmlBody.classList.add('off')
+//   })
 
-  modalEntranceClose.addEventListener('click', () => {
-    entranceBox.classList.remove('modal--show'),
-      htmlBody.classList.remove('off')
-  })
+//   modalEntranceClose.addEventListener('click', () => {
+//     entranceBox.classList.remove('modal--show'),
+//       htmlBody.classList.remove('off')
+//   })
+// })
+
+// new modal func
+
+document.querySelector('.header-cell__btn-input').addEventListener('click', () => {
+  document.querySelector('#modalEntrance').classList.add('modal--show')
 })
 
+
+
+const back = document.querySelector('.modal-back')
+const box = document.querySelector('.modal-box')
+
+back.addEventListener('click', (e) => {
+  if (e.target.classList.contains(box)) {
+    console.log('click')
+  }
+})
