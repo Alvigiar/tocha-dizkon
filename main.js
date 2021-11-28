6710687a4846233df06317b03dc2d6ba734035a0
Alvigiar <alvigiar.hildrison@gmail.com>
2021-11-28 17:35:04 +0300

    fixed

--- main.js
+++ main.js
@@ -5,137 +5,144 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-Swiper.use([Navigation, Pagination]);
+document.addEventListener('DOMContentLoaded', () => {
 
-const burgerBtn = document.querySelector('.header-burger')
-const htmlBody = document.querySelector('body')
-const headContent = document.querySelector('.header-content')
-const burgerToggle = document.querySelector('.header-burger__toggle')
-const btnEntranceOpen = document.querySelector('.header-cell__btn-input')
-const btnRegisterOpen = document.querySelector('.header-cell__btn-reg')
-const modalBox = document.querySelector('.modal')
-const btnModalClose = document.querySelectorAll('.modal-box__btn-close')
-const widthScroll = window.innerWidth - document.body.offsetWidth
+  Swiper.use([Navigation, Pagination]);
 
-var swiperTariff = new Swiper(".swiperTariff", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2
+  const burgerBtn = document.querySelector('.header-burger')
+  const htmlBody = document.querySelector('body')
+  const burgCross = document.querySelector('.header-burger__toggle')
+  const headContent = document.querySelector('.header-content')
+  const burgerToggle = document.querySelector('.header-burger__toggle')
+  const btnOpenModalEntrance = document.querySelector('#open-entrance')
+  const btnOpenModalRegister = document.querySelector('#open-register')
+
+  const widthScroll = window.innerWidth - document.body.offsetWidth
+
+  var swiperTariff = new Swiper(".swiperTariff", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    1440: {
-      slidesPerView: 3
-    }
-  },
-});
-
-var swiperConnection = new Swiper(".swiperConnection", {
-  slidesPerView: 1.5,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 3
+    breakpoints: {
+      768: {
+        slidesPerView: 2
+      },
+      1440: {
+        slidesPerView: 3
+      }
     },
-  },
-});
+  });
 
-var swiperRevievs = new Swiper(".swiperRevievs", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 1.5
+  var swiperConnection = new Swiper(".swiperConnection", {
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    1440: {
-      slidesPerView: 2
+    breakpoints: {
+      768: {
+        slidesPerView: 3
+      },
     },
-  },
-})
+  });
 
-burgerBtn.addEventListener('click', () => {
-  headContent.classList.toggle('active'),
-    htmlBody.classList.toggle('off'),
-    burgerToggle.classList.toggle('cross')
-})
-
-btnEntranceOpen.addEventListener('click', () => {
-  let entranceOpen = new Modal(htmlBody, headContent, burgerToggle, modalBox)
-  entranceOpen.modalEntranceOpen()
-})
-
-btnRegisterOpen.addEventListener('click', () => {
-  let registerOpen = new Modal(htmlBody, headContent, burgerToggle, modalBox)
-  registerOpen.modalRegisterOpen()
-})
-
-btnModalClose.forEach(element => {
-  element.addEventListener('click', () => {
-    let modalClose = new Modal(htmlBody, headContent, burgerToggle, modalBox)
-    modalClose.modalClose()
+  var swiperRevievs = new Swiper(".swiperRevievs", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 1.5
+      },
+      1440: {
+        slidesPerView: 2
+      },
+    },
   })
-})
 
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
-    let btnModalClose = new Modal(htmlBody, headContent, burgerToggle, modalBox)
-    btnModalClose.modalClose()
+  btnOpenModalEntrance.addEventListener('click', () => {
+    const modalEntrance = document.querySelector('#modal-entrance')
+    const modalOpen = new Modal(modalEntrance, burgCross, headContent, htmlBody)
+    modalOpen.open()
+  })
+
+  btnOpenModalRegister.addEventListener('click', () => {
+    const modalRegister = document.querySelector('#modal-register')
+    const modalOpen = new Modal(modalRegister, burgCross, headContent, htmlBody)
+    modalOpen.open()
+  })
+
+  class Modal {
+    constructor(modalBox, burgCross, headContent, htmlBody) {
+      this.modal = modalBox
+      this.modal.addEventListener('click', (e) => {
+        if (e.target.classList.contains('modal--show')) {
+          this.close()
+        }
+      })
+      this.btnClose = modalBox.querySelector('.modal__btn-close')
+      this.btnClose.addEventListener('click', () => {
+        this.close()
+      })
+      this.cross = burgCross
+      this.head = headContent
+      this.body = htmlBody
+    }
+
+    open() {
+      this.modal.classList.add('modal--hide')
+      this.cross.classList.remove('cross')
+      this.head.classList.remove('menu--active')
+      this.body.classList.add('off')
+      setTimeout(() => {
+        this.modal.classList.add('modal--show')
+      }, 0);
+    }
+
+    close() {
+      this.modal.classList.add('modal--hide')
+      this.modal.classList.remove('modal--show')
+      this.body.classList.remove('off')
+      setTimeout(() => {
+        this.modal.classList.remove('modal--hide')
+      }, 500);
+    }
+  }
+
+  burgerBtn.addEventListener('click', () => {
+    const burg = new Menu(htmlBody, burgCross, headContent)
+
+    if (burgerToggle.classList.contains('cross')) {
+      burg.close()
+    } else {
+      burg.open()
+    }
+  })
+
+  class Menu {
+    constructor(body, cross, head) {
+      this.body = body
+      this.cross = cross
+      this.head = head
+    }
+
+    open() {
+      this.body.classList.add('off')
+      this.cross.classList.add('cross')
+      this.head.classList.add('menu--active')
+    }
+
+    close() {
+      this.body.classList.remove('off')
+      this.cross.classList.remove('cross')
+      this.head.classList.remove('menu--active')
+    }
   }
 })
-
-modalBox.addEventListener('click', (e) => {
-  if (e.target == document.querySelector('.modal')) {
-    let btnModalClose = new Modal(htmlBody, headContent, burgerToggle, modalBox)
-    btnModalClose.modalClose()
-  }
-})
-
-class Modal {
-  constructor(htmlBody, headContent, burgerToggle, modalBox) {
-    this.htmlBody = htmlBody
-    this.headContent = headContent
-    this.burgerToggle = burgerToggle
-    this.modalBox = modalBox
-    this.entranceBox = document.querySelector('.entrance')
-    this.registerBox = document.querySelector('.register')
-    this.btnClose = document.querySelectorAll('.modal-box__btn-close')
-  }
-
-  modalEntranceOpen() {
-    this.entranceBox.classList.add('modal-entrance--show')
-    this.htmlBody.classList.add('off')
-    this.headContent.classList.remove('active')
-    this.burgerToggle.classList.remove('cross')
-    this.htmlBody.style.paddingRight = widthScroll
-    modalBox.classList.add('modal--show')
-  }
-
-  modalRegisterOpen() {
-    this.registerBox.classList.add('modal-register--show')
-    this.htmlBody.classList.add('off')
-    this.headContent.classList.remove('active')
-    this.burgerToggle.classList.remove('cross')
-    this.htmlBody.style.paddingRight = widthScroll
-    modalBox.classList.add('modal--show')
-  }
-
-  modalClose() {
-    this.entranceBox.classList.remove('modal-entrance--show')
-    this.registerBox.classList.remove('modal-register--show')
-    this.htmlBody.classList.remove('off')
-    this.htmlBody.style.paddingRight = ''
-    modalBox.classList.remove('modal--show')
-  }
-}
